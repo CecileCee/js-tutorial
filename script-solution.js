367c0d7c6baf42a884cc77d21c54610551e24e32
CecileCee <cecile.muloway@gmail.com>
2016-07-04 11:34:24 +0200

    solution 4 done

--- script-solution.js
+++ script-solution.js
@@ -101,3 +101,68 @@ function doubleAll(numbers) {
 }
 
 module.exports = doubleAll;
+
+
+
+
+// --------------------------------------------------------------
+//
+// 4. -- Task
+//
+// Use Array#filter to write a function called getShortMessages.
+//
+// getShortMessages takes an array of objects with '.message' properties and returns an array of messages that are less than < 50 characters long.
+//
+// The function should return an array containing the messages themselves, without their containing object.
+//
+// ## Arguments
+//
+//   * messages: an Array of 10 to 100 random objects that look something like this:
+//
+//     {
+//       message: 'Esse id amet quis eu esse aute officia ipsum.' // random
+//     }
+//
+// ## Conditions
+//
+//   * Do not use any for/while loops or Array#forEach.
+//   * Do not create any unnecessary functions e.g. helpers.
+//
+// ## Hint
+//
+//   * Try chaining some Array methods!
+//
+// ## Example
+//
+//     [ 'Tempor quis esse consequat sunt ea eiusmod.',
+//       'Id culpa ad proident ad nulla laborum incididunt.',
+//       'Ullamco in ea et ad anim anim ullamco est.',
+//       'Est ut irure irure nisi.' ]
+//
+// ## Resources
+//
+//   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+//   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+//
+//
+// --------------------------------------------------------------
+
+function getShortMessages(messages) {
+  // sélectionner les messages les plus courts
+  var shortmsgs = [],
+      index = 0,
+      msgsaved = '';
+
+  function isolateMsg(message) {
+    msgsaved = messages[index].message;   // isoler le message;
+    index +=1;                            // incrémenter l'index pour se déplacer
+    if (msgsaved.length <= 50) {          // condition :
+      return shortmsgs.push(msgsaved);    // push seulement les phrases de -5O caractères
+    }
+  }
+
+  messages.map(isolateMsg);               // fonction map car interdiction boucle for
+  return shortmsgs;                       // return du tableau contenant les phrases de -50 caractères
+}
+
+module.exports = getShortMessages;
